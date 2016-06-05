76e22179b594c890a0df47459ec6209dae72cb9e
makyca <sunmaky@gmail.com>
2016-06-06 01:16:09 +0200

    Create Power-Mod_Power.py

---
+++ Power-Mod_Power.py
@@ -0,0 +1,8 @@
+#Task 
+#You are given three integers: a, b, and m, respectively. Print two lines. 
+#The first line should print the result of pow(a,b). The second line should print the result of pow(a,b,m).
+a = int(raw_input())
+b = int(raw_input())
+m = int(raw_input())
+print pow(a,b)
+print pow(a,b,m)

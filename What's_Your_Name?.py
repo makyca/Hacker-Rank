846013810044f6eda8a9330a46bd1981b40acca3
makyca <sunmaky@gmail.com>
2016-06-06 01:21:51 +0200

    Create What's_Your_Name?.py

---
+++ What's_Your_Name?.py
@@ -0,0 +1,9 @@
+#Task
+#You are given the first and last name of a person. 
+#Your task is to read them and print the following:
+#Hello firstname lastname! You just delved into python.
+#Input Format 
+#The first line contains the first name, and the second line contains the last name.
+s1 = raw_input()
+s2 = raw_input()
+print "Hello "+s1+" "+s2+"! You just delved into python."

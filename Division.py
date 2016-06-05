50a396931481f367c7f4dbd094dfc15c288832b9
makyca <sunmaky@gmail.com>
2016-06-06 01:10:57 +0200

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,10 @@
+#Task 
+#Read two integers and print two lines. The first line should contain integer division, a//b. 
+#The second line should contain float division, a/b.
+#Input Format 
+#The first line contains the first integer, a. The second line contains the second integer, b.
+from __future__ import division
+a = int(raw_input())
+b = int(raw_input())
+print a//b
+print a/b
